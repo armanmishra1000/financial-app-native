31835abdec2397f93e5c13f2e269d9d0e7b30a37
armanmishra1000 <armanmishra1111@gmail.com>
2025-12-03 11:20:12 +0530

    Optimize APK size: remove unused Skia, enable ProGuard, clean console logs

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,11 @@ module.exports = function (api) {
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
-    plugins: ["react-native-reanimated/plugin"], // Must be last
+    plugins: [
+      ...(process.env.NODE_ENV === "production"
+        ? [["transform-remove-console", { exclude: ["error", "warn"] }]]
+        : []),
+      "react-native-reanimated/plugin",
+    ], // Must be last
   };
 };
